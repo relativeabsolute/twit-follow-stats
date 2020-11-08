3698ca596bb891be6265211d90c2ba430d942d1d
Johan Burke <jburke4126@gmail.com>
2020-11-08 17:42:39 -0500

    header styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
     providers: [],
     bootstrap: [AppComponent],
 })
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+@NgModule({
+    imports: [CommonModule, MatToolbarModule, MatButtonModule],
+    exports: [MatToolbarModule, MatButtonModule],
+    declarations: [],
+})
+export class MaterialModule {}
