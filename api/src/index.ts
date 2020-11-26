4a1b4fe22dc0c662e9777abe08926c354d83bfc8
Johan Burke <jburke4126@gmail.com>
2020-11-26 12:32:51 -0500

    added .env action

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT ?? 8080;
 const corsOrigin = process.env.CORS_ORIGIN;
 
 const corsOptions = {
